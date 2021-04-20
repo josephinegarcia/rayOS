5e827ed1ba8779967917f77fe69d2bdcc8fa7ca6
GrantPedersen2 <grantpedersen2@live.com>
2021-04-20 16:56:09 -0700

    Initial Paging Support

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,11 @@
 #include <kernel/serial.h>
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
-int kernel_main() {
+#include <kernel/multiboot.h>
+
+#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
+
+int kernel_main(multiboot_info_t* mbd, unsigned int magic){
 	gdt_install();
 	idt_install();
 	isrs_install();
@@ -10,6 +14,15 @@ int kernel_main() {
 	__asm__ __volatile__ ("sti");
 	keyboard_install();
 	terminal_initialize();
-	// printf("Hello WORLD!!!");
+
+	if(MULTIBOOT_BOOTLOADER_MAGIC != magic)
+		printf ("Invalid magic number: 0x%x\n", (unsigned) magic);
+
+	 /* Are mem_* valid? */
+  	if (CHECK_FLAG (mbd->flags, 0))
+    	printf ("mem_lower = %uKB, mem_upper = %uKB\n", (unsigned) mbd->mem_lower, (unsigned) mbd->mem_upper);
+
+	printf("MADE IT!");
+
 	return 0;
 }
